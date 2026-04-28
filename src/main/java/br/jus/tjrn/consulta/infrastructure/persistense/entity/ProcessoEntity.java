1be372268fe7f7723d6a644cfd6ccc0a41568d7b
Yan Ferreira <yanfvc@gmail.com>
2026-04-28 00:42:23 -0300

    feat: Adiciona controller para busca de todos os processos

--- src/main/java/br/jus/tjrn/consulta/infrastructure/persistense/entity/ProcessoEntity.java
+++ src/main/java/br/jus/tjrn/consulta/infrastructure/persistense/entity/ProcessoEntity.java
@@ -1,5 +1,7 @@
 package br.jus.tjrn.consulta.infrastructure.persistense.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -18,10 +20,22 @@ public class ProcessoEntity {
 
     @Column(name = "nr_processo")
     private String numero;
+
+    // @Column(name = "cd_processo_status")
+    // private String status;
+
+    // @Column(name = "dt_autuacao", columnDefinition = "TIMESTAMP")
+    // private LocalDateTime dataAutuacao;
     
     @Column(name = "nr_processo_origem")
     private String numeroProcessoOrigem;
+
+    // @Column(name = "in_segredo_justica")
+    // private boolean segredoJustica;
+
+    // @Column(name = "ds_classe_judicial")
+    // private String classeJudicial;
+
+
     
-    @Column(name = "ds_complemento")
-    private String descricaoComplemento;
 }
---
+++ src/main/java/br/jus/tjrn/consulta/interfaces/rest/controller/ProcessoController.java
@@ -0,0 +1,54 @@
+package br.jus.tjrn.consulta.interfaces.rest.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.jus.tjrn.consulta.application.usecase.ConsultarProcessoUseCase;
+import br.jus.tjrn.consulta.domain.model.Processo;
+import br.jus.tjrn.consulta.domain.model.ProcessoFiltro;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/processos")
+@RequiredArgsConstructor
+public class ProcessoController {
+
+    private final ConsultarProcessoUseCase consultarProcessoUseCase;
+
+    @GetMapping
+    public List<Processo> consultar(
+        @RequestParam(required = false) String numero,
+        @RequestParam(required = false) String numeroReferencia,
+        @RequestParam(required = false) String nomeParte,
+        @RequestParam(required = false) String nomeAdvogado,
+        @RequestParam(required = false) String classeJudicial,
+        @RequestParam(required = false) String cpfCnpj,
+        @RequestParam(required = false) String oab,
+        @RequestParam(required = false) String ufoab,
+        @RequestParam(required = false) LocalDate dataInicio,
+        @RequestParam(required = false) LocalDate dataFim
+    ) {
+        
+        ProcessoFiltro filtro = ProcessoFiltro.builder()
+            .numero(numero)
+            .numeroReferencia(numeroReferencia)
+            .nomeParte(nomeParte)
+            .nomeAdvogado(nomeAdvogado)
+            .classeJudicial(classeJudicial)
+            .cpfCnpj(cpfCnpj)
+            .oab(oab)
+            .ufOab(ufoab)
+            .dataInicio(dataInicio)
+            .dataFim(dataFim)
+            .build();
+
+        return consultarProcessoUseCase.executar(filtro);
+    }
+
+
+}
